fc79c05b996ceef5a9710432962e77b52f7ca3e9
Lewis Foskett <2661355f@student.gla.ac.uk>
2023-01-18 13:40:17 +0000

    completed exercises for 3.6

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+from django.utils.html import format_html
+
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    link = '/rango/about/'
+    return HttpResponse("Rango says hey there partner!" + format_html('<a href="{}">About</a>', link))
 
+
+def about(request):
+    link = '/rango/'
+    return HttpResponse("Rango says here is the about page." + format_html('<a href="{}">Index</a>', link))
